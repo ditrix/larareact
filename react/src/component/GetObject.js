f62657eb3c68f22acf6a92d3674081d641d93dd7
ditrix <dmvoloshin@gmail.com>
2019-12-27 11:55:55 +0200

    +fictive components

---
+++ react/src/component/GetObject.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react' 
+
+// ввод даних про об'ект страхування
+class GetObject extends Component {
+    render(){
+        return(
+            <span>GetObject</span>
+        )
+    }
+}
+
+export default GetObject
---
+++ react/src/component/PayValue.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react' 
+
+// ввод даних про об'ект страхування
+class PayValue extends Component {
+    render(){
+        return(
+            <span>PayValue</span>
+        )
+    }
+}
+
+export default PayValue
