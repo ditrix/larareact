c46b9687d98acfeebe14517ccebbe8755e50bbd0
ditrix <dmvoloshin@gmail.com>
2020-02-11 15:02:57 +0200

    data matrix dgo

---
+++ react/src/data/calcMarix.js
@@ -0,0 +1,141 @@
+// TODO: global after refactoring  
+const parametersOSGPO = {
+    k1: [
+        { valuePar:"B1", valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+        { valuePar:"B2", valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+        { valuePar:"B3", valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+        { valuePar:"B4", valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+        { valuePar:"B5", valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+        { valuePar:"F",  valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+        { valuePar:"D1", valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+        { valuePar:"D2", valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+        { valuePar:"C1", valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+        { valuePar:"C2", valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+        { valuePar:"A1", valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+        { valuePar:"A2", valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+    ],
+    k2: [
+        { valuePar:"1", valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+        { valuePar:"2", valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+        { valuePar:"3", valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+        { valuePar:"4", valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+        { valuePar:"5", valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+        { valuePar:"6", valueValc:1, valueK6:1, isActive:1, descrRU: '', descrUA:''  },
+    ]
+}
+
+const dataDGO1 = [
+    {   
+        insutSum:'100000',
+        paysMatrix:[
+            {zone:'1', valuePl:'180'},
+            {zone:'2', valuePl:'140'},
+            {zone:'3', valuePl:'130'},
+            {zone:'4', valuePl:'100'},
+            {zone:'5', valuePl:'90'},
+            {zone:'6', valuePl:'200'},
+        ]
+    },
+    {   
+        insutSum:'200000',
+        paysMatrix:[
+            {zone:'1', valuePl:'290'},
+            {zone:'2', valuePl:'230'},
+            {zone:'3', valuePl:'210'},
+            {zone:'4', valuePl:'170'},
+            {zone:'5', valuePl:'150'},
+            {zone:'6', valuePl:'320'},
+        ]
+    },
+    {   
+        insutSum:'300000',
+        paysMatrix:[
+            {zone:'1', valuePl:'390'},
+            {zone:'2', valuePl:'310'},
+            {zone:'3', valuePl:'280'},
+            {zone:'4', valuePl:'230'},
+            {zone:'5', valuePl:'200'},
+            {zone:'6', valuePl:'420'},
+        ]
+    },
+    {   
+        insutSum:'400000',
+        paysMatrix:[
+            {zone:'1', valuePl:'472'},
+            {zone:'2', valuePl:'372'},
+            {zone:'3', valuePl:'340'},
+            {zone:'4', valuePl:'270'},
+            {zone:'5', valuePl:'240'},
+            {zone:'6', valuePl:'500'},
+        ]
+    },
+    {   
+        insutSum:'500000',
+        paysMatrix:[
+            {zone:'1', valuePl:'540'},
+            {zone:'2', valuePl:'420'},
+            {zone:'3', valuePl:'390'},
+            {zone:'4', valuePl:'300'},
+            {zone:'5', valuePl:'270'},
+            {zone:'6', valuePl:'550'},
+        ]
+    },
+]
+
+const dataDGO2 = [
+    {   
+        insutSum:'100000',
+        paysMatrix:[
+            {zone:'1', valuePl:'422'},
+            {zone:'2', valuePl:'320'},
+            {zone:'3', valuePl:'290'},
+            {zone:'4', valuePl:'200'},
+            {zone:'5', valuePl:'150'},
+            {zone:'6', valuePl:'470'},
+        ]
+    },
+    {   
+        insutSum:'200000',
+        paysMatrix:[
+            {zone:'1', valuePl:'600'},
+            {zone:'2', valuePl:'490'},
+            {zone:'3', valuePl:'440'},
+            {zone:'4', valuePl:'340'},
+            {zone:'5', valuePl:'230'},
+            {zone:'6', valuePl:'670'},
+        ]
+    },
+    {   
+        insutSum:'300000',
+        paysMatrix:[
+            {zone:'1', valuePl:'760'},
+            {zone:'2', valuePl:'620'},
+            {zone:'3', valuePl:'560'},
+            {zone:'4', valuePl:'430'},
+            {zone:'5', valuePl:'285'},
+            {zone:'6', valuePl:'840'},
+        ]
+    },
+    {   
+        insutSum:'400000',
+        paysMatrix:[
+            {zone:'1', valuePl:'900'},
+            {zone:'2', valuePl:'720'},
+            {zone:'3', valuePl:'660'},
+            {zone:'4', valuePl:'500'},
+            {zone:'5', valuePl:'320'},
+            {zone:'6', valuePl:'980'},
+        ]
+    },
+    {   
+        insutSum:'500000',
+        paysMatrix:[
+            {zone:'1', valuePl:'1000'},
+            {zone:'2', valuePl:'800'},
+            {zone:'3', valuePl:'725'},
+            {zone:'4', valuePl:'550'},
+            {zone:'5', valuePl:'350'},
+            {zone:'6', valuePl:'1100'},
+        ]
+    },
+]
--- react/src/data/cityds.js
+++ react/src/data/cityds.js
@@ -4,398 +4,398 @@ export const cityDS = [
     "nameRU": "--место регистрации владельца ТС--",
     "nameUA": "--місце реєстрації власника ТЗ--",
     "zone": "0",
-    "zone_dgo": "0"
+    "zone_dgo": "0","zone_dgo_askods": "4"
   },
   {
     "id": "2",
     "nameRU": "Алчевск",
     "nameUA": "Алчевськ",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "12",
     "nameRU": "Белая Церковь",
     "nameUA": "Біла Церква",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "16",
     "nameRU": "Бердянск",
     "nameUA": "Бердянськ",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "24",
     "nameRU": "Борисполь",
     "nameUA": "Бориспіль",
     "zone": "2",
-    "zone_dgo": "2"
+    "zone_dgo": "2","zone_dgo_askods": "1"
   },
   {
     "id": "25",
     "nameRU": "Боярка",
     "nameUA": "Боярка",
     "zone": "2",
-    "zone_dgo": "2"
+    "zone_dgo": "2","zone_dgo_askods": "1"
   },
   {
     "id": "26",
     "nameRU": "Бровары",
     "nameUA": "Бровари",
     "zone": "2",
-    "zone_dgo": "2"
+    "zone_dgo": "2","zone_dgo_askods": "1"
   },
   {
     "id": "262",
     "nameRU": "Буча",
     "nameUA": "Буча",
     "zone": "2",
-    "zone_dgo": "1"
+    "zone_dgo": "1","zone_dgo_askods": "1"
   },
   {
     "id": "31",
     "nameRU": "Васильков",
     "nameUA": "Васильків",
     "zone": "2",
-    "zone_dgo": "2"
+    "zone_dgo": "2","zone_dgo_askods": "1"
   },
   {
     "id": "34",
     "nameRU": "Винница",
     "nameUA": "Вінниця",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "42",
     "nameRU": "Вышгород",
     "nameUA": "Вишгород",
     "zone": "2",
-    "zone_dgo": "2"
+    "zone_dgo": "2","zone_dgo_askods": "1"
   },
   {
     "id": "263",
     "nameRU": "Вишневое",
     "nameUA": "Вишневе",
     "zone": "2",
-    "zone_dgo": "2"
+    "zone_dgo": "2","zone_dgo_askods": "1"
   },
   {
     "id": "49",
     "nameRU": "Горловка",
     "nameUA": "Горлівка",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "54",
     "nameRU": "Днепр",
     "nameUA": "Дніпро",
     "zone": "2",
-    "zone_dgo": "3"
+    "zone_dgo": "3","zone_dgo_askods": "2"
   },
   {
     "id": "58",
     "nameRU": "Донецк",
     "nameUA": "Донецьк",
     "zone": "3",
-    "zone_dgo": "3"
+    "zone_dgo": "3","zone_dgo_askods": "3"
   },
   {
     "id": "62",
     "nameRU": "Евпатория",
     "nameUA": "Євпаторія",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "64",
     "nameRU": "Житомир",
     "nameUA": "Житомир",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "69",
     "nameRU": "Запорожье",
     "nameUA": "Запоріжжя",
     "zone": "3",
-    "zone_dgo": "3"
+    "zone_dgo": "3","zone_dgo_askods": "3"
   },
   {
     "id": "76",
     "nameRU": "Ивано-Франковск",
     "nameUA": "Івано-Франківськ",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "80",
     "nameRU": "Ирпень",
     "nameUA": "Ірпінь",
     "zone": "2",
-    "zone_dgo": "2"
+    "zone_dgo": "2","zone_dgo_askods": "1"
   },
   {
     "id": "87",
     "nameRU": "Каменец-Подольский",
     "nameUA": "Кам'янець-Подільський",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "89",
     "nameRU": "Каменское",
     "nameUA": "Кам'янське",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "93",
     "nameRU": "Керчь",
     "nameUA": "Керч",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "94",
     "nameRU": "Киев",
     "nameUA": "Київ",
     "zone": "1",
-    "zone_dgo": "1"
+    "zone_dgo": "1","zone_dgo_askods": "1"
   },
   {
     "id": "95",
     "nameRU": "Кропивницкий",
     "nameUA": "Кропивницький",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "110",
     "nameRU": "Краматорск",
     "nameUA": "Краматорськ",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "118",
     "nameRU": "Кременчуг",
     "nameUA": "Кременчук",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "119",
     "nameRU": "Кривой Рог",
     "nameUA": "Кривий Ріг",
     "zone": "3",
-    "zone_dgo": "3"
+    "zone_dgo": "3","zone_dgo_askods": "3"
   },
   {
     "id": "125",
     "nameRU": "Лисичанск",
     "nameUA": "Лисичанськ",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "129",
     "nameRU": "Луганск",
     "nameUA": "Луганськ",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "131",
     "nameRU": "Луцк",
     "nameUA": "Луцьк",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "132",
     "nameRU": "Львов",
     "nameUA": "Львів",
     "zone": "2",
-    "zone_dgo": "2"
+    "zone_dgo": "2","zone_dgo_askods": "2"
   },
   {
     "id": "137",
     "nameRU": "Макеевка",
     "nameUA": "Макіївка",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "139",
     "nameRU": "Мариуполь",
     "nameUA": "Маріуполь",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "143",
     "nameRU": "Мелитополь",
     "nameUA": "Мелітополь",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "156",
     "nameRU": "Николаев",
     "nameUA": "Миколаїв",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "157",
     "nameRU": "Никополь",
     "nameUA": "Нікополь",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },  
   {
     "id": "278",
     "nameRU": "Обухов",
     "nameUA": "Обухів",
     "zone": "2",
-    "zone_dgo": "1"
+    "zone_dgo": "1","zone_dgo_askods": "1"
   },
   {
     "id": "170",
     "nameRU": "Одесса",
     "nameUA": "Одеса",
     "zone": "2",
-    "zone_dgo": "2"
+    "zone_dgo": "2","zone_dgo_askods": "2"
   },
   {
     "id": "173",
     "nameRU": "Павлоград",
     "nameUA": "Павлоград",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "183",
     "nameRU": "Полтава",
     "nameUA": "Полтава",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "191",
     "nameRU": "Ровно",
     "nameUA": "Рівне",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "200",
     "nameRU": "Севастополь",
     "nameUA": "Севастополь",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "201",
     "nameRU": "Северодонецк",
     "nameUA": "Сєвєродонецьк",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "203",
     "nameRU": "Симферополь",
     "nameUA": "Сімферополь",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "206",
     "nameRU": "Славянск",
     "nameUA": "Слов'янськ",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "218",
     "nameRU": "Сумы",
     "nameUA": "Суми",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "220",
     "nameRU": "Тернополь",
     "nameUA": "Тернопіль",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "225",
     "nameRU": "Ужгород",
     "nameUA": "Ужгород",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "231",
     "nameRU": "Харьков",
     "nameUA": "Харків",
     "zone": "2",
-    "zone_dgo": "2"
+    "zone_dgo": "2","zone_dgo_askods": "2"
   },
   {
     "id": "232",
     "nameRU": "Херсон",
     "nameUA": "Херсон",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "234",
     "nameRU": "Хмельницкий",
     "nameUA": "Хмельницький",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "238",
     "nameRU": "Черкассы",
     "nameUA": "Черкаси",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "239",
     "nameRU": "Чернигов",
     "nameUA": "Чернігів",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "240",
     "nameRU": "Черновцы",
     "nameUA": "Чернівці",
     "zone":"4",
-    "zone_dgo": "5"
+    "zone_dgo": "5","zone_dgo_askods": "4"
   },
   {
     "id": "279",
     "nameRU": "другие населённые пункты",
     "nameUA": "інші населені пункти",
     "zone": "5",
-    "zone_dgo": "6"
+    "zone_dgo": "6","zone_dgo_askods": "5"
   },
   {
     "id": "68",
     "nameRU": "За Пределами Украины",
     "nameUA": "Зa Межами України",
     "zone": "6",
-    "zone_dgo": "6"
+    "zone_dgo": "6","zone_dgo_askods": "6"
   }
 ]
